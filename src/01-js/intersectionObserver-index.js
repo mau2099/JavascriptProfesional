128fb2d6aa91057e166856f5dfd65ca118bcd484
Mauricio Garcia <mau200729@gmail.com>
2019-09-24 18:37:14 -0500

    se agregan intersectionobserver y visibilityChange

---
+++ src/01-js/intersectionObserver-index.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/extensions */
+import MediaPlayer from './intersectionObserver-MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()]
+});
+
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
